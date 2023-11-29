2ed188ec86a30fad149726d30b121d8885e1c60f
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-29 18:48:35 +0100

    Refactoring & error cleanup.

--- src/comps/Header.tsx
+++ src/comps/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
                 <Navbar/>
                 <h2>C O C K T A I L<br/>
                     C O M P A S S</h2>
-                <img className='logo' src="./src/assets/Logo-white.png" height={250} alt="Cocktail Compass Logo"/>
+                <img className='logo' src={"src/assets/Logo-white.png"} height={250} alt="Cocktail Compass Logo"/>
             </section>
         </>
     )
